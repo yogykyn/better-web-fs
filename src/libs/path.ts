7301bcb6cb92e9b5b381e38dea2d87cfe50f4256
yogykyn <yogykyn45@gmail.com>
2024-06-24 22:00:32 +0800

    Added resolve, convertURLToPath and convertPathLikeToString functions and fixed a bug in the normalize function caused by variable i and array being out of sync

--- src/libs/path.ts
+++ src/libs/path.ts
@@ -1,19 +1,27 @@
+import type { PathLike } from '../types';
+import { TypeErrorWithCode } from './errors';
+
 export const pathSeperator = '/';
+
+function isAbsolutePath(path: string) {
+  return path.length === 0 || path[0] === '/';
+}
 
 /**
  * Function to normalize the given path.
  */
 export function normalize(path: string): string {
-  const pathSegments = path.split(pathSeperator);
+  const newPath: string[] = [];
+  const pathSegments: string[] = path.split(pathSeperator);
 
   for (let i = 0; i < pathSegments.length; i++) {
     const pathSegment = pathSegments[i];
 
-    if (pathSegment === '..') pathSegments.splice(i - 1, 2);
-    else if (pathSegment === '.') pathSegments.splice(i, 1);
+    if (pathSegment === '..') newPath.pop();
+    else if (pathSegment !== '.' && !(pathSegment.trim())) newPath.push(pathSegment);
   }
 
-  return pathSegments.join(pathSeperator);
+  return newPath.join(pathSeperator);
 }
 
 /**
@@ -30,4 +38,38 @@ export function join(...paths: string[]): string {
   }
 
   return normalize(fullpath);
+}
+
+/**
+ * Function to resolve the given path.
+ */
+export function resolve(cwd: string, ...paths: string[]): string {
+  const fullpath = join(...paths);
+  return '/' + normalize(isAbsolutePath(fullpath) ? fullpath : join(cwd, fullpath));
+}
+
+/**
+ * Function to convert URL (file:/....) to path.
+ */
+export function convertURLToPath (url: URL) {
+  if (url.protocol !== 'file:') {
+    throw new TypeErrorWithCode('[ERR_INVALID_FILE_URL_PATH]', `Expected protocol is file, but given is ${url.protocol}`);
+  }
+
+  if (url.hostname !== '') {
+    throw new TypeErrorWithCode('[ERR_INVALID_FILE_URL_PATH]', 'The URL must be of scheme file');
+  }
+
+  return normalize(url.pathname);
+}
+
+/**
+ * Function to convert path like into path.
+ */
+export function convertPathLikeToString(targetPath: PathLike): string {
+  return normalize(
+    targetPath instanceof URL
+      ? convertURLToPath(targetPath)
+      : targetPath.toString()
+  );
 }
